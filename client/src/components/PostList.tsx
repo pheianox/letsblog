1f974e821489d4ea02d1f4d1348811b455af64d2
pheianox <pheianox@gmail.com>
2022-08-05 00:15:11 +0400

    Fix

--- client/src/components/PostList.tsx
+++ client/src/components/PostList.tsx
@@ -10,23 +10,27 @@ export default function PostList() {
   }, []);
 
   return (
-    <div className="list">
-      {posts.map(({ title, content, id }) => (
-        <div className="card w-96 bg-base-100 shadow-xl" key={id}>
-          <figure></figure>
-          <div className="card-body">
-            <h2 className="card-title">
-              {title}
-              <div className="badge badge-secondary">NEW</div>
-            </h2>
-            <p>{content.slice(0, 50)}...</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">Fashion</div>
-              <div className="badge badge-outline">Products</div>
+    <div className="grid ">
+      {posts.length <= 0 ? (
+        <progress className="progress w-56"></progress>
+      ) : (
+        posts.map(({ title, content, id }) => (
+          <div className="card w-96 bg-base-100 shadow-xl" key={id}>
+            <figure></figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                {title}
+                <div className="badge badge-secondary">NEW</div>
+              </h2>
+              <p>{content.slice(0, 50)}...</p>
+              <div className="card-actions justify-end">
+                <div className="badge badge-outline">Fashion</div>
+                <div className="badge badge-outline">Products</div>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 }
